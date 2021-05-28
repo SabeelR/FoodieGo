86cd554871340c12a8eb395631ff29041c269417
SabeelR <sabeelurahman05@gmail.com>
2021-05-29 00:07:11 +0530

    Initial commit

---
+++ app/src/main/java/com/FoodIndia/FoodieGo/activity/ProfileActivity.kt
@@ -0,0 +1,13 @@
+package com.FoodIndia.FoodieGo.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.FoodIndia.FoodieGo.R
+
+class ProfileActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+    }
+}
---
+++ app/src/main/java/com/FoodIndia/FoodieGo/model/OrderHistoryRestaurant.kt
@@ -0,0 +1,8 @@
+package com.FoodIndia.FoodieGo.model
+
+data class OrderHistoryRestaurant (
+    var orderId:String,
+    var restaurantName:String,
+    var totalCost:String,
+    var orderPlacedAt:String
+)
